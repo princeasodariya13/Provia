776f7c9c942d94f9c212caefe9be9fb7bd8a8e9c
Prince Asodariya <princeasodariya13@gmail.com>
2026-08-21 21:24:58 +0530

    style: add micro-animations, glassmorphism utilities, and interactive depth to Provia UI components

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,11 +1,19 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => (
+export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  interactive?: boolean
+}
+
+const Card = React.forwardRef<HTMLDivElement, CardProps>(
+  ({ className, interactive, ...props }, ref) => (
     <div
       ref={ref}
-      className={cn("border border-border bg-background text-text-primary", className)}
+      className={cn(
+        "border border-border bg-background text-text-primary",
+        interactive && "card-hover-depth cursor-pointer hover:border-brand-hover",
+        className
+      )}
       {...props}
     />
   )
